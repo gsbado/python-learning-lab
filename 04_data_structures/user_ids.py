a4ab35fac88716f5b55f0c283f49f70bf577602a
Gabriela Bado <gabibado92@gmail.com>
2026-06-09 17:44:23 +0000

    update user_ids

--- 04_data_structures/user_ids.py
+++ 04_data_structures/user_ids.py
@@ -1,12 +1,15 @@
 group_a = [101, 102, 103, 104, 105, 102, 103]
 group_b = [103, 104, 106, 107, 108, 104]
 
-groups_diff_a = set(group_a) - set(group_b)
-groups_diff_b = set(group_b) - set(group_a)
-groups_union = set (group_a) & set(group_b)
-groups_intersection = len(set(group_a) | set(group_b))
+set_group_a = set(group_a)
+set_group_b = set(group_b)
+
+groups_diff_a = set_group_a - set_group_b
+groups_diff_b = set_group_b - set_group_a
+groups_union = set_group_a & set_group_b
+groups_unique = len(set_group_a | set_group_b)
 
 print(f"Users only on group A: {groups_diff_a}")
 print(f"Users only on group B: {groups_diff_b}")
 print(f"Users on both groups: {groups_union}")
-print(f"Users unique between the 2 groups: {groups_intersection}")
+print(f"Users unique between the 2 groups: {groups_unique}")
