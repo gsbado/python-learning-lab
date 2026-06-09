40d6ada6275660207a3679e18fbc4c0222fb44b5
Gabriela Bado <gabibado92@gmail.com>
2026-06-09 17:00:49 +0000

    update store.py

--- 04_data_structures/store.py
+++ 04_data_structures/store.py
@@ -10,14 +10,14 @@ product_store = {
 print("====== GABI'S STORE ======\n")
 print("Welcome to our store. This is our principle product today: \n")
 print("Name: " + product_store["name"])
-print(f"Price: {product_store['price']}")
-print(f"How many do we have? {product_store['storage']}")
+print(f"Price: {product_store["price"]}")
+print(f"How many do we have? {product_store["storage"]}")
 
 product_store["price"] = 9.99
 product_store["category"] = "cakes"
 
 print("\n We are on sale today! \n")
 print("Name: " + product_store["name"])
-print(f"New price: {product_store['price']}")
-print(f"How many do we have? {product_store['storage']}")
+print(f"New price: {product_store["price"]}")
+print(f"How many do we have? {product_store["storage"]}")
 print("What is the category of the product: " + product_store["category"])
